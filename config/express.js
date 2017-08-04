8f60dfd887a9d5ae02cd0e2425077059469474f6
Vicente Neto <sneto.vicente@gmail.com>
2017-08-04 00:27:39 -0300

    Creating the initial structure of a Node application with MongoDB

---
+++ config/express.js
@@ -0,0 +1,54 @@
+var glob = require('glob');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var compression = require('compression');
+var methodOverride = require('method-override');
+
+module.exports = function (app, config) {
+  var env = process.env.NODE_ENV || 'development';
+  app.locals.ENV = env;
+  app.locals.ENV_DEVELOPMENT = env === 'development';
+
+  app.use(logger('dev'));
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({
+    extended: true
+  }));
+  app.use(cookieParser());
+  app.use(compression());
+  app.use(methodOverride());
+
+  var controllers = glob.sync(config.root + '/app/controllers/*.js');
+  controllers.forEach(function (controller) {
+    require(controller)(app);
+  });
+
+  app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+  });
+
+  if (app.get('env') === 'development') {
+    app.use(function (err, req, res) {
+      res.status(err.status || 500);
+      res.render('error', {
+        message: err.message,
+        error: err,
+        title: 'error'
+      });
+    });
+  }
+
+  app.use(function (err, req, res) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: {},
+      title: 'error'
+    });
+  });
+
+  return app;
+};
