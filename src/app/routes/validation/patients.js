a97b0d5aef1aa8c6314f753b4ccecc5b1dd4d710
Vicente Neto <sneto.vicente@gmail.com>
2017-08-05 19:33:33 -0300

    Updating Patient listing service to enable email filtering

--- src/app/routes/validation/patients.js
+++ src/app/routes/validation/patients.js
@@ -4,7 +4,7 @@ export default {
   // POST /api/patients
   create: {
     body: {
-      email: joi.string().regex(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/).required(),
+      email: joi.string().regex(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/).required(),
       name: joi.string().required(),
       dateOfBirth: joi.date().iso().required(),
       location: joi.string().required(),
