384edb8276b75d3459e3703ed1525307efaa2742
Vicente Neto <sneto.vicente@gmail.com>
2017-08-06 21:24:54 -0300

    Refactoring schedules service to use possible dates from db

--- src/app.js
+++ src/app.js
@@ -3,11 +3,32 @@ import mongoose from 'mongoose';
 
 import app from './config/express'
 import config from './config/config';
+import Schedule from './app/models/schedule'
 
 mongoose.Promise = bluebird;
 mongoose.connect(config.db, {useMongoClient: true});
 mongoose.connection.on('error', () => {
   throw new Error('unable to connect to database at ' + config.db);
+});
+
+Schedule.find().then((schedules) => {
+  if (schedules.length === 0) {
+    Schedule.create({date: new Date('2017-09-02T08:00:00.000+0000')});
+    Schedule.create({date: new Date('2017-09-02T10:00:00.000+0000')});
+    Schedule.create({date: new Date('2017-09-02T12:00:00.000+0000')});
+    Schedule.create({date: new Date('2017-09-09T08:00:00.000+0000')});
+    Schedule.create({date: new Date('2017-09-09T10:00:00.000+0000')});
+    Schedule.create({date: new Date('2017-09-09T12:00:00.000+0000')});
+    Schedule.create({date: new Date('2017-09-16T08:00:00.000+0000')});
+    Schedule.create({date: new Date('2017-09-16T10:00:00.000+0000')});
+    Schedule.create({date: new Date('2017-09-16T12:00:00.000+0000')});
+    Schedule.create({date: new Date('2017-09-23T08:00:00.000+0000')});
+    Schedule.create({date: new Date('2017-09-23T10:00:00.000+0000')});
+    Schedule.create({date: new Date('2017-09-23T12:00:00.000+0000')});
+    Schedule.create({date: new Date('2017-09-30T08:00:00.000+0000')});
+    Schedule.create({date: new Date('2017-09-30T10:00:00.000+0000')});
+    Schedule.create({date: new Date('2017-09-30T12:00:00.000+0000')});
+  }
 });
 
 app.listen(config.port, () => {
